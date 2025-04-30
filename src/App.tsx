71cbabe0458a9a6ca071565c7da8a91b537423d0
René Torres <knrtorres@carozzicorp.com>
2025-04-30 17:00:21 -0400

    hola

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://carozzicorp-my.sharepoint.com/:b:/p/knrtorres/ER2Dlob9JD9Ng8o4_hb8b7gBZcpb0Ag1SrZlvBnrylyTQQ?e=W5NBig" target="_blank">
+        </a>
+      </div>
+      <p className="read-the-docs">
+        Interandinos
+      </p>
+    </>
+  )
+}
+
+export default App
